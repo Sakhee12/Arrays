77d267b36e7e021b257cdea8e710fb5903ffd5d7
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-07-08 12:30:58 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,34 @@
+package arrays;
+
+public class Array1 
+{
+	public static void main(String[] args) 
+	{
+		int ar [] = new int [3];
+		System.out.println(ar);
+		
+		double [] ar2 = new double [4];
+		System.out.println(ar2);
+		
+		char [] ar3 = new char [5];
+		System.out.println(ar3);
+		
+		String [] ar4 = new String [6];
+		System.out.println(ar4);
+		
+		boolean [] ar5 = new boolean [5];
+		System.out.println(ar5);
+		
+		long [] ar6 = new long [4];
+		System.out.println(ar6);
+		
+		short [] ar7 = new short [5];
+		System.out.println(ar7);
+		
+		byte [] ar8 = new byte[2];
+		System.out.println(ar8);
+		
+		
+		
+	}
+}
