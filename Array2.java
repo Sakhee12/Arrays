97e8e14e89748b9251b5e1f7b13bb1c854802bd7
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-07-08 12:31:36 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,48 @@
+package arrays;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		
+		int [] ar = new int [3];
+		System.out.println(ar[0]);
+		System.out.println(ar[1]);
+		System.out.println(ar[2]);
+		/*
+		 * System.out.println(ar[3]);
+		   System.out.println(ar[-1]);
+		   
+		   	WE CAN NOT PRINT INDEX OUT OF LENGTH
+		 */
+		
+		boolean [] ar2 = new boolean [4];
+		System.out.println(ar2[0]);
+		System.out.println(ar2[1]);
+		System.out.println(ar2[2]);
+		System.out.println(ar2[3]);
+		
+		
+		float [] ar3 = new float [3];
+		System.out.println(ar3[0]);
+		System.out.println(ar3[2]);
+		System.out.println(ar3[1]);
+		
+		String [] ar4 = new String [4];
+		System.out.println(ar4[0]);
+		System.out.println(ar4[1]);
+		System.out.println(ar4[3]);
+		System.out.println(ar4[2]);
+		System.out.println(ar4[2]);
+		
+		
+		char [] ar5 = new char [2];
+		System.out.println(ar5[0]);
+		System.out.println(ar5[1]);
+		
+		
+		
+		
+		
+		
+	}
+}
