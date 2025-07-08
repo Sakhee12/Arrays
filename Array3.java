80e6ad90a2e6523a5ac7d0039dfa76f478075de2
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-07-08 12:31:54 +0530

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,69 @@
+package arrays;
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		
+		int [] ar = new int [2];
+		ar[0] = 10;
+		ar[1] = 20;
+		
+		double [] ar2 = new double [3];
+		ar2[0] = 1.24;
+		ar2[1] = 2.46;
+		ar2[2] = 5.14;
+		
+		boolean [] ar3 = new boolean [4];
+		ar3[0] = true;
+		ar3[1] = false;
+		ar3[2] = true;
+		
+		String [] ar4 = new String [3];
+		ar4[0] = "Raju";
+		ar4[1] = "Kaju";
+		ar4[2] = "Juju";
+		
+		System.out.println(ar.length);
+		System.out.println(ar2.length);
+		System.out.println(ar3.length);
+		System.out.println(ar4.length);
+		
+		for(int i=0; i<ar.length ; i++)
+		{
+			System.out.println(ar[i]);
+		}
+		
+		for(int i=0; i<ar2.length ; i++)
+		{
+			System.out.println(ar2[i]);
+		}
+		
+		for(int i=0; i<ar3.length ; i++)
+		{
+			System.out.println(ar3[i]);
+		}
+		
+		for(int i=0; i<ar4.length ; i++)
+		{
+			System.out.println(ar4[i]);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
